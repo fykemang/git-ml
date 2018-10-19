3120c2ad121191243a0d436ce977150f1e73c320
Mike Fang <mf647@cornell.edu>
2018-10-18 23:21:57 -0400

    Initialized repository with starting files

---
+++ authors.mli
@@ -0,0 +1,9 @@
+(**
+   Metadata about this submission.
+   @author Mike Fang mf647, Ahad Rizvi ar727, Xucheng Wang xw354,
+           Yuqing Cui yc2296
+*)
+
+(** [hours_worked] is a list of the number of hours each team member
+    worked on this assignment. *)
+val hours_worked : int list
---
+++ test.ml
@@ -0,0 +1,5 @@
+open OUnit2
+
+let suite = "TEST SUITE" >::: List.flatten []
+
+let _ = run_test_tt_main suite
