ac5f2ea9159e53f99f43cd73c62e07c556310a06
Mike Fang <mf647@cornell.edu>
2018-10-21 17:10:04 -0400

    merge

--- commands.mli
+++ commands.mli
@@ -3,6 +3,6 @@
 val init : unit -> unit
 
 (** [hash_object s] is the digest of the given string or file after being
-    hashed using the MD5 algorithm *)
+    hashed using the MD5 algorithm.  *)
 val hash_object : string -> unit
 
