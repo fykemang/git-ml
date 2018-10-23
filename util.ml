086dce1a8a8530bc6a0b5f2798d48ebfe87805b2
Ahad Rizvi <ar727@cornell.edu>
2018-10-22 23:54:11 -0400

    tracer for cat_file_to_git_object

--- util.ml
+++ util.ml
@@ -1,6 +1,22 @@
+open GitTree
 
 let hash_file file = if Sys.file_exists file 
   then file |> Digest.file |> Digest.to_hex 
   else raise (Unix.Unix_error (Unix.ENOENT, "hash_file", file))
 
 let hash_str s = s |> Digest.string |> Digest.to_hex
+
+(** [tree_content_to_file_list pointer] is the file list of type 
+    [string*string list] that is a list of filenames and file contents. 
+    Mutually recurisve with [cat_file_to_git_object s] 
+    Requires: 
+      pointer is a valid pointer to a tree*)
+let tree_content_to_file_list (pointer:string) =
+  failwith "Unimplemented"
+
+let cat_file_to_git_object (s:string) =
+  match String.split_on_char ' ' s with
+  |h::t when h = "Blob" -> Blob (List.fold_left (^) "" t )
+  |h::t when h = "Tree_Object" -> tree_content_to_file_list 
+                                    (List.fold_left (^) "" t )
+  |_ -> failwith "Unimplemented"
