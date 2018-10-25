f7c725c05a6f5a65d4e6d6bbe8b33b15fe4ea5d1
Mike Fang <mf647@cornell.edu>
2018-10-24 22:01:22 -0400

    fixes

--- test.ml
+++ test.ml
@@ -1,10 +1,6 @@
 open OUnit2
-open Parse
 
-
-
-let parse_tests = [
-
+let suite = "TEST SUITE" >::: List.flatten [
 ]
-let suite = "TEST SUITE" >::: List.flatten []
+
 let _ = run_test_tt_main suite
